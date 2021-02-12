f99ee56912ff6a3fc51935aca0f0dffa1de49d32
ChrisRRobbers <robbers1996@hotmail.com>
2021-02-12 17:11:31 +0100

    final frontend build for launch

--- public/js/index.js
+++ public/js/index.js
@@ -98,7 +98,6 @@ if (item) {
         const swiper1 = new Swiper('.swiper1', {
           slidesPerView: 1,
           spaceBetween: 80,
-          loop: true,
           pagination: {
             el: '.swiper-pagination',
             clickable: true,
